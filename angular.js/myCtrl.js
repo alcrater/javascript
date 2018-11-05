2dec33bd0b6bab050b1a3cb19d1a079c9d683b00
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-05 08:41:56 -0500

    update

--- angular.js/myCtrl.js
+++ angular.js/myCtrl.js
@@ -1,4 +1,15 @@
 app.controller("myCtrl", function($scope) {
     $scope.firstName = "Audrey";
     $scope.lastName= "Crater";
-}); 
+ 
+
+$http.get("json_sample.html").then(function (response) {
+    $scope.myData = response.data.records;
+
+  });
+
+  $http.get("table.php").then(function (response) {
+    $scope.myDatas = response.data.records;
+
+    });
+});
