e685193d88b3f8b47b8687ee566829978aae886c
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-05 08:38:35 -0500

    update

--- angular.js/table.php
+++ angular.js/table.php
@@ -4,13 +4,13 @@ header("Content-Type: application/json; charset=UTF-8");
 
 $conn = new mysqli("localhost", "audrey", "southhills#", "audrey");
 
-$result = $conn->query("SELECT userid, first_name, last_name, city FROM angular_people");
+$result = $conn->query("SELECT first_name, last_name, city FROM angular_people");
 
 $outp = "";
 while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
     if ($outp != "") {$outp .= ",";}
-	$outp .= '("First Name":"'   . $rs["first_name"]    	. '",';
-	$outp .= '"Last Name":"'. $rs["last_name"] 	. '"}';
+	$outp .= '{"First Name":"'   . $rs["first_name"]    	. '",';
+	$outp .= '"Last Name":"'. $rs["last_name"] 	. '"';
   $outp .= '"City":"'. $rs["city"] 	. '"}';
 }
 $outp ='{"records":['.$outp.']}';
