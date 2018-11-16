0c798efc21f27e4ee8a693715f0b432f13431d06
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-15 21:03:08 -0500

    update

--- angular.js/testingtodo/dbconnection.php
+++ angular.js/testingtodo/dbconnection.php
@@ -4,7 +4,7 @@
 $db_host = 'localhost'; //database is install on php server
 $db_user = 'audrey'; //name to log into mysql
 $db_password = 'southhills#'; //password to login to mysql
-$db_name = 'todo'; //name of the database within mysql
+$db_name = 'audrey'; //name of the database within mysql
 $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
 if ($conn->connect_error) {
     die("Connection failed: ". $conn->connect_error);
