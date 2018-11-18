3f162c586fd8657ff0873f69a6e303642262affb
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-18 17:05:36 -0500

    updates

--- angular.js/myCtrl.js
+++ angular.js/myCtrl.js
@@ -1,10 +1,57 @@
-app.controller("myCtrl", function($scope, $http) {
-    $scope.firstName = "Audrey";
-    $scope.lastName= "Crater";
-    $http.get("json.html").then(function (response) {
-    $scope.myData = response.data.records;
-    });
-    $http.get("table.php").then(function (response) {
-    $scope.myPHPData = response.data.records;
-    })
-    });
+app.controller('myCtrl', function($scope, $http) {
+
+    getTask(); // Load all available tasks 
+  
+    function getTask(){  
+  
+    $http.post("gettask.php").success(function(data){
+  
+          $scope.tasks = data;
+  
+         });
+  
+    };
+  
+    $scope.addTask = function (task) {
+  
+      $http.post("addtask.php?task="+task).success(function(data){
+  
+          getTask();
+  
+          $scope.taskInput = "";
+  
+        });
+  
+    };
+  
+    $scope.deleteTask = function (task) {
+  
+      if(confirm("Are you sure to delete this line?")){
+  
+      $http.post("deletetask.php?taskID="+task).success(function(data){
+  
+          getTask();
+  
+        });
+  
+      }
+  
+    };
+  
+  
+  
+    $scope.toggleStatus = function(item, status, task) {
+  
+      if(status=='2'){status='0';}else{status='2';}
+  
+        $http.post("updatetask.php?taskID="+item+"&status="+status).success(function(data){
+  
+          getTask();
+  
+        });
+  
+    };
+  
+  
+  
+  });
