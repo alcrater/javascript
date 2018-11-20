6b3e603fe014a77b3b76a105fb91f156b9cdf624
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-19 20:01:16 -0500

    update

--- taskapp.php
+++ taskapp.php
@@ -3,31 +3,3 @@
 var app = angular.module('taskapp', []);
 
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-
-  require('dbconnection.php');
-
-	$sql = "SELECT * FROM todo";
-
-	$checkForTable = $conn->query($sql);
-
-	if (mysqli_num_rows($checkForTable) < 1 ) {
-
-    CREATE TABLE IF NOT EXISTS `todo` (
-
-      `id` int(11) NOT NULL AUTO_INCREMENT,
-    
-      `task` varchar(200) NOT NULL,
-    
-      `status` int(11) NOT NULL,
-    
-      `created_at` int(11) NOT NULL,
-    
-      PRIMARY KEY (`id`)
-    
-    );
-
-    $tableCreate = $conn->query($sql);
-
-  }
-
