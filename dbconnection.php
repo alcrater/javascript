e5f7488a6bf2ff5e6a69efe5c3ae50760ad0ae66
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-19 19:29:07 -0500

    update

--- dbconnection.php
+++ dbconnection.php
@@ -12,35 +12,5 @@ if ($conn->connect_error) {
 
 
 
-USE angularcode_task;
-
-CREATE TABLE IF NOT EXISTS `todo` (
-
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-
-  `task` varchar(200) NOT NULL,
-
-  `status` int(11) NOT NULL,
-
-  `created_at` int(11) NOT NULL,
-
-  PRIMARY KEY (`id`)
-
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
-
-
-
-
-INSERT INTO `tasks` (`id`, `task`, `status`, `created_at`) VALUES
-
-(1, 'My first task', 0, 1390815970),
-
-(2, 'Task Two', 2, 1390815993),
-
-(3, 'Find Three', 2, 1390817659),
-
-(4, 'Find Four', 2, 1390818389);
-
-
 ?>
 
