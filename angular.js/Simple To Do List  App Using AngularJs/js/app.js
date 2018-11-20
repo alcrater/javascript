2dfe3af654f49c4a48ffd5cee2634eb8f5941a77
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-19 20:16:05 -0500

    update

--- angular.js/Simple To Do List  App Using AngularJs/js/app.js
+++ angular.js/Simple To Do List  App Using AngularJs/js/app.js
@@ -1,20 +1,20 @@
 angular.module('myModule', [])
 				.controller('myController', ['$scope', function($scope){
 					$scope.lists =[
-						{name: 'Creating A Database', done: false},
-						{name: 'Creating A Database Connection', done: false},
-						{name: 'Creating Webpage Layout', done: false},
-						{name: 'Adding Some Php Script', done: false},
-						{name: 'Adding Javascript Function', done: false},
-						{name: 'Need Testing', done: false},
+						{name: 'this is for testing', done: false},
+						{name: 'Google for Help', done: false},
+						{name: 'found a website to help with this task', done: false},
+						{name: 'Add Angular JS and PHP', done: false},
+						{name: 'Took way to long to get this, many frustrating hours', done: false},
+						{name: 'I give up', done: false},
 					];
 					
-					$scope.addList = function(){
+					$scope.addTask = function(){
 						$scope.lists.push({name: $scope.newlist, done: false})
 						$scope.newlist = "";
 					}
 					
-					$scope.clearList = function(){
+					$scope.clearTask = function(){
 						$scope.lists = $scope.lists.filter(function(item){
 							return !item.done;
 						});
