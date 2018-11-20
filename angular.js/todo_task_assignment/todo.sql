dcf853f90030514225607acce3626e66440d2044
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-20 10:37:20 -0500

    update

--- angular.js/todo_task_assignment/todo.sql
+++ angular.js/todo_task_assignment/todo.sql
@@ -1,11 +1,11 @@
 
 CREATE TABLE IF NOT EXISTS `todo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `item` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `item` varchar(200) NOT NULL,
   `status` int(11) NOT NULL,
   `created_at` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=37 ;
+);
 
 
 
