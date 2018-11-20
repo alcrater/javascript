5ea59a1e77680f188f17b389265e79c4e80cef94
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-20 10:53:48 -0500

    update

--- angular.js/todo_task_assignment/includes/db.php
+++ angular.js/todo_task_assignment/includes/db.php
@@ -7,8 +7,23 @@ $db_password = 'southhills#'; //password to login to mysql
 $db_name = 'audrey'; //name of the database within mysql
 $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
 
+if ($conn->connect_error)
+{
+  die("Connection Failed: " . $conn->connect_error);
+}
+
+if(isset($_GET['task'])){
+$task = $_GET['task'];
+$status = "0";
+$created = time();
+$query="INSERT INTO tasks(task,status,created_at)  VALUES ('$task', '$status', '$created')";
+$result = $mysqli->query($query);
+$result = $mysqli->affected_rows;
+echo $json_response = json_encode($result);
+}
 
 ?>
+
 
 
 
