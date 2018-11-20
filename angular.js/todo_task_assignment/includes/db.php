d332cba7f7a55ce225b040e04655d40bad83795d
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-20 10:51:36 -0500

    update

--- angular.js/todo_task_assignment/includes/db.php
+++ angular.js/todo_task_assignment/includes/db.php
@@ -7,12 +7,7 @@ $db_password = 'southhills#'; //password to login to mysql
 $db_name = 'audrey'; //name of the database within mysql
 $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
 
-$_POST = json_decode(file_get_contents('ajax/addItem'), true);
-  $newTask = $_POST['item'];
-  //Create Query to Select All user data from fm_users Table
-  $sql = "INSERT INTO todo (item) VALUES ('$newTask')";
-  //Execute the SQL and Return Array of Values to $result
-  $result = $conn->query($sql);
+
 ?>
 
 
